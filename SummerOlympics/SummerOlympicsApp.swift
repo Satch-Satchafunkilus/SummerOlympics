a69b128ee6234753c0f4901baf24c1305d99e2cf
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-06-19 04:06:19 -0500

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by Tushar Munge on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
